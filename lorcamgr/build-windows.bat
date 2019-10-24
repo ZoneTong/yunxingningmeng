bb538c5b7b76163fd836ef5d5ddaaf5a537e7893
zht <zhoutong.zht@gmail.com>
2019-10-24 17:03:56 +0800

    使用说明书

--- lorcamgr/build-windows.bat
+++ lorcamgr/build-windows.bat
@@ -1,8 +1,8 @@
 @echo off
 
 echo pack begin
-xcopy icons\ningmeng.ico www\ 	/Y 
-go generate
+:: xcopy icons\ningmeng.ico www\ 	/Y 
+:: go generate
 :: rm www\ningmeng.ico
 go build -ldflags "-H windowsgui -X main.TRIAL_DAY=21 -X 'main.BUILD_TIME=%date:~0,4%%date:~5,2%%date:~8,2%'"  -o yunxing.exe
 :: go build -ldflags "-H windowsgui -X main.TRIAL_DAY=2 -X 'main.BUILD_TIME=20191023'"  -o yunxing.exe
